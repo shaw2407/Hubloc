5dd4064eec7184d50ccbeea0ce7c3b0b8ae922df
shaw2407 <cchandlerdave@gmail.com>
2020-07-28 23:16:35 +0800

    as
    ASa

--- main.js
+++ main.js
@@ -10,10 +10,3 @@ menuOpen.addEventListener('click', function (){
 menuClose.addEventListener('click', function (){
     nav.classList.toggle('open-nav');
 });
-
-
-// for(let i=1;i<3;i++){
-//     const div = document.createElement('div');
-//     div.id='nani';
-//     servingsMenu.appendChild(div);
-// }
